d5a7cf116f106411497baf2278d842e190ffb4f1
Apurv <apple@Apples-MacBook-Pro.local>
2026-03-17 13:18:50 +0530

    hardcode api url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5001',
+  baseURL: import.meta.env.VITE_API_URL || 'https://taskflow-backend-2foh.onrender.com'
 })
 
 API.interceptors.request.use((config) => {
